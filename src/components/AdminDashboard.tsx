7bec0c2152ac15d08b161da0c2937a46b32e48a0
Luis Calle <luedu1103@gmail.com>
2024-10-15 22:55:40 -0500

    no more ads

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import '../index.css';
 
 const AdminDashboard: React.FC = () => {
-  const [ads, setAds] = useState<any[]>([]);
+  const [notifications, setNotifications] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true); // State for loading spinner
   const [error, setError] = useState<string | null>(null); // State for error handling
 
@@ -20,7 +20,7 @@ const AdminDashboard: React.FC = () => {
           },
         });
         if (Array.isArray(response.data)) {
-          setAds(response.data);
+          setNotifications(response.data);
         } else {
           console.error('Unexpected data format:', response.data);
           setError('Unexpected data format received from the server.');
@@ -41,7 +41,7 @@ const AdminDashboard: React.FC = () => {
       return;
     }
 
-    const adToDelete = ads[index];
+    const adToDelete = notifications[index];
     const response = await fetch(`https://api.contigosanmarcos.com/panel/notifications/${adToDelete.id}`, {
       method: 'DELETE',
       headers: {
@@ -50,7 +50,7 @@ const AdminDashboard: React.FC = () => {
     });
   
     if (response.ok) {
-      setAds((prevAds) => prevAds.filter((_, i) => i !== index));
+      setNotifications((prevNotifications) => prevNotifications.filter((_, i) => i !== index));
       alert(`Se borró con éxito el banner`);
     } else {
       alert(`Necesitas iniciar sesión primero`);
@@ -64,25 +64,25 @@ const AdminDashboard: React.FC = () => {
       ) : error ? (
         <p className="error-message">{error}</p> // Display error message if any
       ) : (
-        <div className="ad-list">
-          {ads.length > 0 ? (
-            ads.map((ad, index) => (
-              <div key={ad.id} className={`ad ad-item ${ad.ad_type}`}>
-                {ad.ad_type === 'banner' ? (
-                  <div className="ad-banner">
-                    {ad.image_url && <img src={ad.image_url} alt={ad.ad_title} />}
+        <div className="notification-list">
+          {notifications.length > 0 ? (
+            notifications.map((notification, index) => (
+              <div key={notification.id} className={`notification notification-item ${notification.ad_type}`}>
+                {notification.ad_type === 'banner' ? (
+                  <div className="notification-banner">
+                    {notification.image_url && <img src={notification.image_url} alt={notification.ad_title} />}
                   </div>
                 ) : (
-                  <div className="ad-post">
-                    {ad.image_url && (
+                  <div className="notification-post">
+                    {notification.image_url && (
                       <div className="ad-post-image">
-                        <img src={ad.image_url} alt={ad.ad_title} />
+                        <img src={notification.image_url} alt={notification.ad_title} />
                       </div>
                     )}
-                    {ad.ad_title && <h3>{ad.ad_title}</h3>}
-                    {ad.ad_content && <p>{ad.ad_content}</p>}
-                    {ad.target_url && (
-                      <a href={ad.target_url} target="_blank" rel="noopener noreferrer">
+                    {notification.ad_title && <h3>{notification.ad_title}</h3>}
+                    {notification.ad_content && <p>{notification.ad_content}</p>}
+                    {notification.target_url && (
+                      <a href={notification.target_url} target="_blank" rel="noopener noreferrer">
                         More Info
                       </a>
                     )}
@@ -100,7 +100,7 @@ const AdminDashboard: React.FC = () => {
               </div>
             ))
           ) : (
-            <p>No ads available.</p>
+            <p>No notifications available.</p>
           )}
         </div>
       )}
